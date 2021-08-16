bf3bd3f304565fefa1f1ba414b0efc131f007e34
Kshitija Gade <jrdev.kshitijagade111@mailji.me>
2021-08-16 18:21:01 +0530

    option change

--- form3jquery.php
+++ form3jquery.php
@@ -27,7 +27,6 @@
 		        <option>Assam</option>
 		        <option>Bihar</option>
 		        <option>Chandigarh</option>
-		        <option>Delhi</option>
 		       
 		    </select>
 		</div>
@@ -72,6 +71,7 @@
 		        <option>444602</option>
 		        <option>444605</option>
 		        <option>444607</option>
+                <option>444608</option>
 		        <option>OTHER</option>
 		    </select>
     	</div>
